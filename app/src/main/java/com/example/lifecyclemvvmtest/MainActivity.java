86e19c854db544a5bc626b5a585c11e0afbe06ee
mohammad <m.zandieh1999@gmail.com>
2021-06-06 13:42:45 +0430

    add without lifecycle

--- app/src/main/java/com/example/lifecyclemvvmtest/MainActivity.java
+++ app/src/main/java/com/example/lifecyclemvvmtest/MainActivity.java
@@ -6,9 +6,26 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyLocationListener myLocationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myLocationListener = new MyLocationListener(this);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        myLocationListener.start();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        myLocationListener.stop();
     }
 }
---
+++ app/src/main/java/com/example/lifecyclemvvmtest/MyLocationListener.java
@@ -0,0 +1,19 @@
+package com.example.lifecyclemvvmtest;
+
+import android.content.Context;
+
+import javax.security.auth.callback.Callback;
+
+class MyLocationListener {
+    public MyLocationListener(Context context) {
+        // ...
+    }
+
+    void start() {
+        // connect to system location service
+    }
+
+    void stop() {
+        // disconnect from system location service
+    }
+}
